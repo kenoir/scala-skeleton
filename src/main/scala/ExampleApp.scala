8a74d2aedcbb25fde80ef9425d10c90d725004cb
Robert Kenny <robert.kenny@guardian.co.uk>
2022-05-10 13:55:03 +0100

    bloody obvious

--- src/main/scala/ExampleApp.scala
+++ src/main/scala/ExampleApp.scala
@@ -1,6 +1,19 @@
 object ExampleApp extends App {
   println("hello world")
   
-  val userInput = scala.io.StdIn.readLine()
-  println(userInput)
+//  val userInput = scala.io.StdIn.readLine()
+//
+//  println(userInput)
+
+  val myList = List("apple", "pear", "lemon", "orange")
+
+  println(myList.take(9))
+
+  val listSize = 10
+
+  val newList = (0 to listSize).collect { n =>
+    myList(n % myList.size)
+  }
+
+  println(newList)
 }
